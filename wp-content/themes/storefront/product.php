3b83d09ee81cfcc2e651f4cbe09bfa756b41d68c
Emil Jönsson <karlperemil@gmail.com>
2017-09-11 15:23:08 +0200

    qtranslate plugin

--- wp-content/themes/storefront/product.php
+++ wp-content/themes/storefront/product.php
@@ -1,14 +1,6 @@
 <?php
-/**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package storefront
- */
+
+/* Template Name: Product */ 
 
 get_header(); ?>
 
@@ -34,5 +26,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-//do_action( 'storefront_sidebar' );
+do_action( 'storefront_sidebar' );
 get_footer();
