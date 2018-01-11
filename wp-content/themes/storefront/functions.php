d2ece4a57e4fcb7ec3e98797a632d74420d9dcf8
Emil <karlperemil@gmail.com>
2018-01-11 14:33:30 +0100

    fix service button

--- wp-content/themes/storefront/functions.php
+++ wp-content/themes/storefront/functions.php
@@ -49,7 +49,7 @@ if ( is_admin() ) {
 	require 'inc/admin/class-storefront-plugin-install.php';
 }
 
-define ('VERSION', '4.9.21');
+define ('VERSION', '4.9.22');
 
 function version_id() {
   if ( WP_DEBUG )
