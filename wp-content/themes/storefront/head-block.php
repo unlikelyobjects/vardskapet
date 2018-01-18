35d3ae6ebb0de9c8d81d210bc7506efa58d5f263
Emil <karlperemil@gmail.com>
2018-01-18 13:56:24 +0100

    analytics tracking code

--- wp-content/themes/storefront/head-block.php
+++ wp-content/themes/storefront/head-block.php
@@ -2,14 +2,15 @@
 <html <?php language_attributes(); ?>>
 <head>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110408257-2"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110408257-1"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-110408257-2');
+  gtag('config', 'UA-110408257-1');
 </script>
+
 
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
