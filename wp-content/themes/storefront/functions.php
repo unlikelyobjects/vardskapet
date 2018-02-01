c0301dd128f6005f154ff0cbe47af9e6f9f95d9c
Emil <karlperemil@gmail.com>
2018-02-01 20:20:13 +0100

    rm excerpt in share

--- wp-content/themes/storefront/functions.php
+++ wp-content/themes/storefront/functions.php
@@ -49,7 +49,7 @@ if ( is_admin() ) {
 	require 'inc/admin/class-storefront-plugin-install.php';
 }
 
-define ('VERSION', '4.9.30');
+define ('VERSION', '4.9.31');
 
 function version_id() {
   if ( WP_DEBUG )
